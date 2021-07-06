242854188d4c7ae3a8a1bafbc8dd4addd5b82af9
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2021-07-06 08:17:36 -0400

    feature/jogadores(C10):implement Service class

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class JogadoresService {}
+export class JogadoresService {
+
+  private jogadores: Jogador[] = [];
+
+  private readonly logger = new Logger(JogadoresService.name)
+
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+
+    this.criar(criarJogadorDto)
+
+  }
+
+  private criar(criarJogadorDto: CriarJogadorDto): void {
+
+    const { nome, email, telefoneCelular } = criarJogadorDto
+
+    const jogador: Jogador = {
+      _id: uuid(),
+      nome,
+      email,
+      telefoneCelular,
+      ranking: 'A',
+      urlFotoJogador: 'www.google.com/foto123.jpg',
+      posicaoRanking: 1
+    }
+
+    this.logger.log(`crarJogadorDto: ${JSON.stringify(jogador)}`)
+    this.jogadores.push(jogador)
+
+  }
+}
